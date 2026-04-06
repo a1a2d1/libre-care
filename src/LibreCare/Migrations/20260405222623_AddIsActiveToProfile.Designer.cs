83a490501977de5d1c13f812a46feaad3deaf7d6
a1a2d1 <96814824+a1a2d1@users.noreply.github.com>
2026-04-05 22:32:17 -0400

    Add IsActive field to the Profile table

--- src/LibreCare/Migrations/20260405222623_AddIsActiveToProfile.Designer.cs
+++ src/LibreCare/Migrations/20260405222623_AddIsActiveToProfile.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LibreCare.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibreCare.Migrations
 {
     [DbContext(typeof(LibreCareContext))]
-    partial class LibreCareContextModelSnapshot : ModelSnapshot
+    [Migration("20260405222623_AddIsActiveToProfile")]
+    partial class AddIsActiveToProfile
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.5");
---
+++ src/LibreCare/Migrations/20260405222623_AddIsActiveToProfile.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibreCare.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsActiveToProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDefault",
+                table: "Profiles",
+                newName: "IsActive");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "Profiles",
+                newName: "IsDefault");
+        }
+    }
+}
